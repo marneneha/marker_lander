070c22a5e68f53e44714f038e2937bb41f1e7ae1
nehamarne27 <nehamarne27gmail,com>
2021-05-08 12:11:35 +0530

    takeoff done

--- include/marker_lander/marker_lander.h
+++ include/marker_lander/marker_lander.h
@@ -6,24 +6,24 @@
 #include <std_srvs/Trigger.h>
 #include <std_srvs/SetBool.h>
 
+	std_srvs::SetBool					  motor_request;
 	mavros_msgs::CommandBool 			  arm_request;
 	mavros_msgs::SetMode 				  srv_setMode;
-    std_srvs::SetBool					  motor_request;
-	std_srvs::Trigger					  srv_takeoff;
-    ros::ServiceClient                    arm_client;
+	std_srvs::Trigger   				  srv_takeoff;
+
     ros::ServiceClient                    motor_client;
-    ros::ServiceClient                    land_client;
+    ros::ServiceClient                    arm_client;
     ros::ServiceClient                    set_mode_client;
-    ros::ServiceClient                    goto_client;
     ros::ServiceClient                    takeoff_client;
+    ros::ServiceClient                    land_client;
 
+    ros::ServiceClient                    goto_client;
 
-    //see uav here need to replace
     std::string motor_service_name = std::string("/")+"uav1"+ "/control_manager/motors";
-    std::string arv_service_name = std::string("/")+"uav1"+ "/mavros/cmd/arming";
-	std::string land_service_name = std::string("/")+"uav1"+ "/mavros/cmd/land";
+    std::string arm_service_name = std::string("/")+"uav1"+ "/mavros/cmd/arming";
 	std::string mode_service_name = std::string("/")+"uav1"+ "/mavros/set_mode";
-	std::string takeoff_service_name = std::string("/")+"uav1"+ "/mavros/takeoff";
+	std::string takeoff_service_name = std::string("/")+"uav1"+ "/uav_manager/takeoff";
+	std::string land_service_name = std::string("/")+"uav1"+ "/mavros/cmd/land";
 
 
 
--- src/marker_lander_node.cpp
+++ src/marker_lander_node.cpp
@@ -5,11 +5,11 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "marker_lander_node");
     ros::NodeHandle nh("~");
 
-	motor_client = nh.serviceClient<std_srvs::SetBool>(motor_service_name);
-	arm_client = (nh.serviceClient<mavros_msgs::CommandBool>(arv_service_name));
-	land_client = (nh.serviceClient<mavros_msgs::CommandTOL>(land_service_name));
+	motor_client = (nh.serviceClient<std_srvs::SetBool>(motor_service_name));
+	arm_client = (nh.serviceClient<mavros_msgs::CommandBool>(arm_service_name));
 	set_mode_client = (nh.serviceClient<mavros_msgs::SetMode>(mode_service_name));
 	takeoff_client = (nh.serviceClient<std_srvs::Trigger>(takeoff_service_name));
+	land_client = (nh.serviceClient<mavros_msgs::CommandTOL>(land_service_name));
 
     takeoff();
 
@@ -20,14 +20,16 @@ int main(int argc, char **argv)
 void takeoff()
 {
 	std::cout << __FILE__ << ":" << __LINE__ << "i am at takeoff start "  <<std::endl; 
-	/*motor_request.request.data = 1;
+	motor_request.request.data = 1;
 	motor_client.call(motor_request);
 	while(!motor_request.response.success)
 	{
 		ros::Duration(.1).sleep();
 		motor_client.call(motor_request);
-	}*/
+	}
 	std::cout << __FILE__ << ":" << __LINE__ << "motor on"  <<std::endl; 
+
+	
 	//set arm
 	ros::Duration(5).sleep();
 	arm_request.request.value = true;
@@ -39,9 +41,11 @@ void takeoff()
 	}
 	std::cout << __FILE__ << ":" << __LINE__ << "arming done "  <<std::endl; 
 	if(arm_request.response.success)
-		{
-			ROS_INFO("Arming Successful");	
-		}
+	{
+		ROS_INFO("Arming Successful");	
+	}
+
+	
 	//set mode
 	ros::Duration(5).sleep();
 	srv_setMode.request.base_mode = 0;
@@ -53,23 +57,26 @@ void takeoff()
 	}else{
 	      ROS_ERROR("Failed SetMode");
 	}
+
+
 	//takeoff
 	if(arm_request.response.success){
 		ros::Duration(5).sleep();
 		takeoff_client.call(srv_takeoff);
 		std::cout << __FILE__ << ":" << __LINE__ << "i got in if after arming"<<"service responce"<<srv_takeoff.response.success<<std::endl; 
-		/*while (!srv_takeoff.response.success)
+		while (srv_takeoff.response.success)
 		{
-		std::cout << __FILE__ << ":" << __LINE__ << "i got in if after arming"<<"service responce"<<srv_takeoff.response.success<<std::endl; 
+			std::cout << __FILE__ << ":" << __LINE__ << "i got in while after arming"<<std::endl; 
+			ros::Duration(.1).sleep();
+			takeoff_client.call(srv_takeoff);
+		}
+		/*for(int i=0;i<200;i++)
+		{
 			ros::Duration(.1).sleep();
 			takeoff_client.call(srv_takeoff);
 		}*/
-		for(int i=0;i<100;i++)
-		{
-		std::cout << __FILE__ << ":" << __LINE__ << "i got in while after arming"  <<std::endl; 
-			takeoff_client.call(srv_takeoff);
-		}
-		if(srv_takeoff.response.success){
+		std::cout << __FILE__ << ":" << __LINE__ << "i got in if after arming"<<"service responce"<<srv_takeoff.response.success<<std::endl; 
+		if(!srv_takeoff.response.success){
 			ROS_INFO("takeoff %d", srv_takeoff.response.success);
 		}else{
 			ROS_ERROR("Failed Takeoff");
